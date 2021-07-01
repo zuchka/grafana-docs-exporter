ca48dc293400cef917c3842805ec0b44e9993369
Matt Abrams <zuchka@protonmail.ch>
2021-06-30 14:29:08 -1000

    added docs sum and fre average metrics

--- exporter.py
+++ exporter.py
@@ -1,4 +1,4 @@
-import time
+import time, re
 from prometheus_client.core import GaugeMetricFamily, REGISTRY
 from prometheus_client import start_http_server
 from script import script
@@ -13,9 +13,18 @@ class CustomCollector(object):
     def collect(self):
         for result in results:
             for item in result:
-                g = GaugeMetricFamily("FleschScore", 'Help text', labels=["url"])
-                g.add_metric([item], result[item])
-                yield g
+                if re.match(r'^https', item):
+                    g = GaugeMetricFamily("flesch_score", 'flesch reading ease score per URL', labels=["url"])
+                    g.add_metric([item], result[item])
+                    yield g
+                elif re.match(r'^flesch_average', item):
+                    gm = GaugeMetricFamily("average_fre", 'overall FRE for docs repo', labels=["average"])
+                    gm.add_metric([item], result[item])
+                    yield gm
+                elif re.match(r'^docs_sum', item):
+                    gs = GaugeMetricFamily("docs_sum", 'total number of docs in /latest', labels=["sum"])
+                    gs.add_metric([item], result[item])
+                    yield gs
 
 
 if __name__ == '__main__':
